e5214bdec31496254db07f1e7489430b35f33807
Sam Osborne <sam.tosborne@googlemail.com>
2020-03-26 00:17:16 +0000

    Use hostname rather than url

--- config.inc.php
+++ config.inc.php
@@ -48,7 +48,7 @@ $dbsys = "pgsql";
 // to use Unix Domain Sockets instead of TCP/IP. For mysql "localhost"
 // tells the system to use Unix Domain Sockets, and $db_port will be ignored;
 // if you want to force TCP connection you can use "127.0.0.1".
-$db_host = getenv('DATABASE_URL');
+$db_host = getenv('DB_HOSTNAME');
 // If you need to use a non standard port for the database connection you
 // can uncomment the following line and specify the port number
 // $db_port = 1234;
