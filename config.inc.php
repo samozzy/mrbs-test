9ed0fec64bf80a5f80f602dcdd6e3b2952e25af7
Sam Osborne <sam.tosborne@googlemail.com>
2020-03-26 00:24:03 +0000

    Use envar properly?

--- config.inc.php
+++ config.inc.php
@@ -42,26 +42,28 @@ $timezone = "Europe/London";
 /*******************
  * Database settings
  ******************/
+$db_heroku = parse_url(getenv("DATABASE_URL"));
+$db_heroku["name"] = ltrim($db["path"], "/");
 // Which database system: "pgsql"=PostgreSQL, "mysql"=MySQL
 $dbsys = "pgsql";
 // Hostname of database server. For pgsql, can use "" instead of localhost
 // to use Unix Domain Sockets instead of TCP/IP. For mysql "localhost"
 // tells the system to use Unix Domain Sockets, and $db_port will be ignored;
 // if you want to force TCP connection you can use "127.0.0.1".
-$db_host = getenv('DB_HOSTNAME');
+$db_host = $db_heroku["host"];
 // If you need to use a non standard port for the database connection you
 // can uncomment the following line and specify the port number
-// $db_port = 1234;
+$db_port = $db_heroku["port"];
 // Database name:
-// $db_database = "mrbs";
+$db_database = $db_heroku["name"];
 // Schema name.  This only applies to PostgreSQL and is only necessary if you have more
 // than one schema in your database and also you are using the same MRBS table names in
 // multiple schemas.
 //$db_schema = "public";
 // Database login user name:
-$db_login = getenv('MRBS_DB_LOGIN');
+$db_login = $db_heroku["user"];
 // Database login password:
-$db_password = getenv('MRBS_DB_PASSWORD');
+$db_password = $db_heroku["pass"];
 //// This will be an environment variable! 
 // Prefix for table names.  This will allow multiple installations where only
 // one database is available
